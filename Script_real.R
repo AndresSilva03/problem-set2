715237988d8d37cc27fea2938c4176488343baf4
AndresSilva03 <af.silvag1@uniandes.edu.co>
2024-04-11 12:19:05 -0500

    Act.

--- Script_real.R
+++ Script_real.R
@@ -18,15 +18,43 @@ p_load(tidyverse, # funciones para manipular/limpiar conjuntos de datos.
 )
 
 
-#1. Importar/Exportar bases de datos:
+# ======= 1. Importar/Exportar bases de datos =========#
 
 
-setwd("C:/Users/silva/OneDrive/Escritorio/Penúltimo Semestre/Taller de R/problemset2")
+# 1.1)
 
 location <- import("Módulo de identificación.dta")
-
 identification <- import("Módulo de sitio o ubicación.dta")
 
-write_rds(location,file = )
 
-help(export)
+# 1.2)
+
+#help(export)
+export(location,"output/location.rds")
+export(identification, "output/identification.rds")
+
+
+#=========== 2. Generar variables ============#
+
+# 2.1)
+
+location <-  mutate(location, businesstype = case_when(GRUPOS4 == "01" ~ "Agricultura",
+                                                       GRUPOS4 == "02" ~ "Industria_Manufaturera",
+                                                       GRUPOS4 == "03" ~ "Comercio",
+                                                       GRUPOS4 == "04" ~ "Servicios"))
+  
+  
+
+  
+# 2.2) 
+
+#P241 es edad según el diccionario
+
+#Dividí los grupos etarios según la clasificación que da el ministerio de salud
+#en su página minsalud.gov.co
+location <- mutate(location, grupo_etario = case_when(P241<=26 ~ "Jovenes",
+                                                      P241>26 & P241<=59 ~ "Adultos",
+                                                      P241>60 ~ "Adulto_Mayor"))  
+
+# 2.3) 
+
